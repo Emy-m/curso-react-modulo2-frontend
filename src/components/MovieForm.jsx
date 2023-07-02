0850546764b217700d88686180698765ffc27415
Emilio Martin <emiunrn@gmail.com>
2023-07-02 17:20:45 -0300

    fin clase 21

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -2,12 +2,28 @@ import { PropTypes } from "prop-types";
 import { useForm } from "react-hook-form";
 import MovieModel from "../../MovieModel";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, MenuItem, Stack, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
 
 MovieForm.propTypes = {
   movie: PropTypes.object,
 };
 
 const imageURL = import.meta.env.VITE_IMAGES_URL;
+
+const StyledTextField = styled(TextField)({
+  "& .MuiInputBase-input": {
+    backgroundColor: "#b3b3b3",
+  },
+  "& .MuiFormLabel-root": {
+    color: "black",
+  },
+  "& .MuiFormHelperText-root.Mui-error": {
+    color: "#a73535",
+    fontWeight: "bold",
+  },
+});
 
 export default function MovieForm({ movie }) {
   const {
@@ -16,6 +32,20 @@ export default function MovieForm({ movie }) {
     formState: { errors },
   } = useForm();
   const nav = useNavigate();
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    getGenres();
+  }, []);
+
+  const getGenres = async () => {
+    const movieModel = new MovieModel();
+    const response = await movieModel.getAllGenres();
+
+    if (response && response.length > 0) {
+      setGenres(response);
+    }
+  };
 
   const onSubmit = (data) => {
     data.id = movie.id;
@@ -32,34 +62,67 @@ export default function MovieForm({ movie }) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register("title", { required: true })}
-        defaultValue={movie.title}
-      />
-      {errors.title && <span>This field is required</span>}
-      <input
-        {...register("year", { required: true })}
-        defaultValue={movie.year}
-      />
-      {errors.year && <span>This field is required</span>}
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
+      display="flex"
+      justifyContent="center"
+    >
+      <Stack spacing={2}>
+        <StyledTextField
+          variant="filled"
+          label="Title"
+          {...register("title", { required: true })}
+          defaultValue={movie.title}
+          error={!!errors.title}
+          helperText={errors.title && "This field is required"}
+        />
 
-      <input
-        accept="image/png"
-        {...register("genre", { required: true })}
-        defaultValue={movie.genre}
-      />
-      {errors.genre && <span>This field is required</span>}
+        <StyledTextField
+          variant="filled"
+          label="Year"
+          {...register("year", { required: true })}
+          defaultValue={movie.year}
+          error={!!errors.year}
+          helperText={errors.year && "This field is required"}
+        />
 
-      <img src={imageURL + "/" + movie.image} />
-      <input
-        type="file"
-        accept="image/png"
-        {...register("image", { required: movie.id ? false : true })}
-      />
-      {errors.image && <span>This field is required</span>}
+        {genres && genres.length > 0 && (
+          <StyledTextField
+            variant="filled"
+            label="Genre"
+            select
+            {...register("genre", { required: true })}
+            defaultValue={movie.genre}
+            error={!!errors.genre}
+            helperText={errors.genre && "This field is required"}
+          >
+            {genres.map((genre) => (
+              <MenuItem key={genre} value={genre}>
+                {genre}
+              </MenuItem>
+            ))}
+          </StyledTextField>
+        )}
 
-      <input type="submit" />
-    </form>
+        <img src={imageURL + "/" + movie.image} />
+        <StyledTextField
+          variant="filled"
+          label="image"
+          type="file"
+          focused
+          accept="image/png"
+          {...register("image", { required: movie.id ? false : true })}
+          error={!!errors.image}
+          helperText={errors.image && "This field is required"}
+        />
+
+        <Button variant="contained" color="success" type="submit">
+          Send
+        </Button>
+      </Stack>
+    </Box>
   );
 }
